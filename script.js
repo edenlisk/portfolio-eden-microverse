64e42715779832f1fea283368384932d163de831
edenlisk <nkumbuyenock@gmail.com>
2022-11-16 16:35:52 +0200

    Fixed mobile menu position & Fixed script.js ESLint errors

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-const nav_item = document.querySelectorAll('.nav-item');
-const horizontal_bar = document.querySelector('.horizontal_bar');
-const top_navigation_bar = document.querySelector('.top-navigation-bar');
-const header_welcome = document.querySelector('.header-welcome');
-const desktop_navigation_bar = document.querySelector('.desktop-nav-container');
-const menuicon = document.querySelector('.menu-icon');
+const navItem = document.querySelectorAll('.nav-item');
+const horizontalBar = document.querySelector('.horizontal_bar');
+const topNavigationBar = document.querySelector('.top-navigation-bar');
+const headerWelcome = document.querySelector('.header-welcome');
+const desktopNavigationBar = document.querySelector('.desktop-nav-container');
+const menuIcon = document.querySelector('.menu-icon');
 const main = document.querySelector('main');
-function restoredefaults(){
-    menuicon.classList.remove('active');
-    header_welcome.classList.remove('active');
-    top_navigation_bar.classList.remove('active');
-    desktop_navigation_bar.classList.remove('active');
-    horizontal_bar.classList.remove('active');
-    for (let item = 0; item < nav_item_Array.length; item++) {
-        nav_item_Array[item].classList.remove('active');
-    }
+const mobileMenuFooter = document.querySelector('.mobile_menu_footer');
+const navItemArray = Array.from(navItem);
+function restoredefaults() {
+  menuIcon.classList.remove('active');
+  headerWelcome.classList.remove('active');
+  topNavigationBar.classList.remove('active');
+  desktopNavigationBar.classList.remove('active');
+  horizontalBar.classList.remove('active');
+  mobileMenuFooter.classList.remove('active');
+  for (let item = 0; item < navItemArray.length; item += 1) {
+    navItemArray[item].classList.remove('active');
+  }
 }
-function mobilemenu(){
-    menuicon.classList.toggle("active");
-    desktop_navigation_bar.classList.toggle('active');
-    header_welcome.classList.toggle('active');
-    top_navigation_bar.classList.toggle('active');
-    horizontal_bar.classList.toggle('active');
-    const  nav_item_Array = Array.from(nav_item);
-    for( let item = 0; item < nav_item_Array.length; item++){
-        nav_item_Array[item].classList.toggle('active');
-    }
+function mobilemenu() {
+  menuIcon.classList.toggle('active');
+  desktopNavigationBar.classList.toggle('active');
+  headerWelcome.classList.toggle('active');
+  topNavigationBar.classList.toggle('active');
+  horizontalBar.classList.toggle('active');
+  mobileMenuFooter.classList.toggle('active');
+  const navItemArray = Array.from(navItem);
+  for (let item = 0; item < navItemArray.length; item += 1) {
+    navItemArray[item].classList.toggle('active');
+  }
 }
-main.addEventListener('click', restoredefaults)
-const nav_item_Array = Array.from(nav_item);
-document.onclick = function (e) {
-    if (e.target === top_navigation_bar) {
-        restoredefaults()
+main.addEventListener('click', restoredefaults);
+document.onclick = (e) => {
+  if (e.target === topNavigationBar) {
+    restoredefaults();
+  }
+};
+navItemArray.forEach((navMenu) => {
+  navMenu.addEventListener('click', restoredefaults);
+  navMenu.addEventListener('keydown', (e) => {
+    if (e.keyCode === 9) {
+      restoredefaults();
     }
-}
-nav_item_Array.forEach((navmenu) =>{
-    navmenu.addEventListener('click', restoredefaults)
-    navmenu.addEventListener('keydown', function (e){
-        if (e.keyCode === 9){
-            restoredefaults()
-        }
-    })
-})
+  });
+});
 
-menuicon.addEventListener('click', mobilemenu)
-menuicon.addEventListener('keydown', function (e){
-    if(e.keyCode === 9){
-        mobilemenu()
-    }
-})
+menuIcon.addEventListener('click', mobilemenu);
+menuIcon.addEventListener('keydown', (e) => {
+  if (e.keyCode === 9) {
+    mobilemenu();
+  }
+});
